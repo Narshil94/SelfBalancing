d790dbf5ac230e2068394279007d7810da1e95fd
Nash <narshil94@gmail.com>
2021-06-19 11:34:17 +0100

    Initial commit

---
+++ Esp_motor_driver/HallEffect/HallEffect.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		HallEffect.ino
+ Created:	6/19/2021 11:19:40 AM
+ Author:	Nash
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
